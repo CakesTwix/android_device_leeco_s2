3ee62892b18dacc689b95f2eaf41f79c0fccfb0a
CakesTwix <oleg.kiryazov@gmail.com>
2020-06-03 19:35:44 +0300

    Revert "s2: RIP Dirac"
    This reverts commit faf3a574134eb0cd0b84047a52b14081d088f76c.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -22,6 +22,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -34,5 +35,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
             if (DEBUG) Log.d(TAG, "Starting Doze service");
             DozeUtils.startService(context);
         }
+        new DiracUtils(context).onBootCompleted();
     }
 }
