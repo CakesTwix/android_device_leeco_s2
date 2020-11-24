e3517444c4923c1631763c74a4f4b8d65452cd60
tImIbreakdown <timifix@gmail.com>
2020-11-24 14:14:51 +0300

    [temporary]: selinux permissive
    Change-Id: Iefc40638e77393b64fb07b5c7b9d595319ba0322

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,6 +52,7 @@ BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci earlyprintk
 BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x00000100
